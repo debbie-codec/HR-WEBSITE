2017d1fb43f512ae3e403b360bb3b35e4639514d
debbie-codec <124357108+debbie-codec@users.noreply.github.com>
2026-07-01 12:46:45 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("menu-toggle");
-const navMenu = document.getElementById("nav-menu");
-
-toggle.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
